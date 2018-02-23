79b233676b8207c7b68ca84f01ad73f85b863011
rkita <rkita4@gmail.com>
2018-02-22 18:40:43 -0800

    updated notebook

--- scripts/eqtl_funcs.py
+++ scripts/eqtl_funcs.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
 
 
+import gtex_funcs as GF
+import ols_funcs as OF
+import scipy.stats
 
-def get_stats(chrNum, pos, gene):
+def get_eqtl_stats(chrNum, pos, geneName, tissue, paramDict):
+    '''
 
+    '''
+    expVector, indivExpVector = GF.get_exp_vector(geneName, tissue, paramDict["gtexExpDir"])
+    genoVector, indivGenoVector = GF.get_dos_vector(chrNum, pos, paramDict["vcfDir"])
+    genoVector = GF.filter_and_sort_genotype_vector(genoVector, indivGenoVector, indivExpVector)
+
+    results = scipy.stats.linregress(genoVector,expVector)
+    residuals = OF.calculate_residuals(genoVector, expVector, results.slope, results.intercept)
+
+    return results.slope, results.intercept, residuals
