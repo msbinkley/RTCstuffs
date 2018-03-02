c435e6b698989f36dac67972c6909f6a8079288d
Michael Binkley <michaelbinkley@DN527ord.SUNet>
2018-03-01 18:58:15 -0800

    updates

--- scripts/eqtl_funcs.py
+++ scripts/eqtl_funcs.py
@@ -15,14 +15,16 @@ def get_eqtl_stats(chrNum, pos, geneName, tissue, paramDict):
 
     '''
     #expVector, indivExpVector = GF.get_exp_vector(geneName, tissue, paramDict["gtexExpDir"])
+
     
     #genoVector, indivGenoVector = GF.get_dos_vector(chrNum, pos, paramDict["vcfDir"])
     expVector, indivExpVector = GF.get_exp_vector(geneName, tissue, paramDict["gtexDir"])
-    
-    genoVector, indivGenoVector = GF.get_dos_vector(chrNum, pos, paramDict["gtexDir"])
-    genoVector = GF.filter_and_sort_genotype_vector(genoVector, indivGenoVector, indivExpVector)
 
+    genoVector, indivGenoVector = GF.get_dos_vector(chrNum, pos, paramDict["gtexDir"])
+
+    genoVector, __ = GF.filter_and_sort_genotype_vector(genoVector, indivGenoVector, indivExpVector)
     results = scipy.stats.linregress(genoVector,expVector)
+
     residuals = OF.calculate_residuals(genoVector, expVector, results.slope, results.intercept)
 
-    return results.slope, results.intercept, residuals
+    return results.slope, results.intercept, residuals, indivExpVector
