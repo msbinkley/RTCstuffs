4a73c02063f0e4e3b050d56a40811d2cdddafcf1
Michael Binkley <michaelbinkley@c02k415zdrvg.attlocal.net>
2018-04-22 16:07:37 -0700

    updates

--- scripts/eqtl_funcs.py
+++ scripts/eqtl_funcs.py
@@ -28,3 +28,22 @@ def get_eqtl_stats(chrNum, pos, geneName, tissue, paramDict):
     residuals = OF.calculate_residuals(genoVector, expVector, results.slope, results.intercept)
 
     return results.slope, results.intercept, residuals, indivExpVector
+
+def get_eqtl_stats_real(chrNum, pos, geneName, tissue, paramDict):
+    '''
+
+    '''
+    #expVector, indivExpVector = GF.get_exp_vector(geneName, tissue, paramDict["gtexExpDir"])
+
+    
+    #genoVector, indivGenoVector = GF.get_dos_vector(chrNum, pos, paramDict["vcfDir"])
+    expVector, indivExpVector = GF.get_exp_vector(geneName, tissue, paramDict["gtexDir"])
+
+    genoVector, indivGenoVector = GF.get_dos_vector(str(chrNum), str(pos), paramDict["gtexDir"])
+
+    genoVector, __ = GF.filter_and_sort_genotype_vector(genoVector, indivGenoVector, indivExpVector)
+    #results = scipy.stats.linregress(genoVector,expVector)
+
+    #residuals = OF.calculate_residuals(genoVector, expVector, results.slope, results.intercept)
+    #results.slope, results.intercept,
+    return  expVector, indivExpVector
