1bc87de4882648757445308c398031d748515795
Michael Binkley <michaelbinkley@DN527s3a.SUNet>
2018-06-28 16:56:32 -0700

    updates

--- scripts/deprecated_funcs.py
+++ scripts/deprecated_funcs.py
@@ -73,3 +73,46 @@ def extract_best_eqtl(paramDict):
 
 
 
+
+
+    
+def extract_best_eqtl_real(paramDict):
+    '''
+    Need a function to then identify the best eqtl for a given gene. Again, this step may not be necessary
+    '''
+    fileIN = open(paramDict["gtexDir"] + '/QTLinterest.txt')    
+    fileIN.readline()
+    longestGeneCoordinates = list()
+    currentLine = fileIN.readline().rstrip().split('\t') 
+    currentGeneCoordinates = [str(currentLine[0]), str(currentLine[2]),float(currentLine[14]), str(currentLine[18]), float(currentLine[23])]#This is a variable that holds the information for the "current" gene.
+    #"Current" gene is the gene that we are currently working to figuring out the longest coordinates.
+    for counter, i in enumerate(fileIN): #Go through each line
+        i = i.rstrip().split("\t")
+        i =[str(i[0]), str(i[2]),str(i[11]), float(i[14]), str(i[18]), float(i[23])]
+
+          
+        if i[0] == currentGeneCoordinates[0]: #If this line has the same gene as "current" SNP, then continue to compare the coordiantes.
+            currentDistance = abs(currentGeneCoordinates[23])
+            thisLinesDistance = abs(i[23])
+            if thisLinesDistance < currentDistance: #If this line is shorter than our "current" gene, then update the "current" gene.
+                currentGeneCoordinates = i
+                
+        if i[0] != currentGeneCoordinates[0]: 
+            #If this line's gene does not match the currentGene's gene, then we must have moved on to the next gene.
+            #Since we're now at a new gene, let's store the currentGene
+            longestGeneCoordinates.append(currentGeneCoordinates )
+            #Also, let's update the currentGene to match this gene. 
+            currentGeneCoordinates = i
+        if counter %100000==0:
+            print(counter)
+
+       
+    fileOUT = open(paramDict["tmpSmall"] + "/besteQTLs_real.txt", "w")
+    for i in longestGeneCoordinates:
+        print(i)
+        fileOUT.write("\t".join([str(x) for x in i]) + "\n")  #Join will take a list, and create ONE string. where each element of the list is separated by the character in the quotation marks (here it is a "\t")
+        #Also,we're converting the items into a string - because we can't output integers. 
+    fileOUT.close() 
+
+
+
